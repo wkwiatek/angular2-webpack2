05c32a9b307efcf073df964d62037212e432fd5b
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2015-12-14 13:30:40 +0100

    Sample style added

--- src/components/test.ts
+++ src/components/test.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'test',
-  template: 'Hello from component: {{ test }}'
+  template: 'Hello from component: {{ test }}',
+  styles: [require('./test.styl').toString()]
 })
 export class TestComponent {
   public test: string = 'test';
