9960bccc48a057449adfb80c8ffee360517fc3b1
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-01-27 15:25:13 +0100

    Webpack config improved

--- src/components/test.ts
+++ src/components/test.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'test',
   template: 'Hello from component: {{ test }}',
-  styles: [require('./test.styl').toString()]
+  styles: [require('./test.styl')]
 })
 export class TestComponent {
   public test: string = 'test';
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ var path = require('path');
 var webpack = require('webpack');
 
 var config = {
-  devtool: 'source-map',
   entry: {
     'vendor': './src/vendor',
-    'app': './src/bootstrap' 
+    'app': './src/bootstrap'
   },
   output: {
-    filename: 'dist/[name].js'   
+    filename: 'dist/[name].js'
   },
   resolve: {
     extensions: ['', '.ts', '.es6', '.js', '.json']
@@ -17,15 +16,19 @@ var config = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
       { test: /\.ts$/, exclude: /node_modules/, loader: 'ts' },
-      { test: /\.html/, loader: 'raw' },
-      { test: /\.styl$/, loader: 'css!stylus' },
-      { test: /\.css$/, loader: 'css' },
-      { test: /\.(gif|png|jpe?g)$/i, loader: 'file' }
+      { test: /\.html/, loader: 'html?minimize=false' },
+      { test: /\.styl$/, loader: 'raw!stylus' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.svg$/, loader: 'svg' },
+      { test: /\.(gif|png|jpe?g)$/i, loader: 'file?name=dist/images/[name].[ext]' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?name=dist/fonts/[name].[ext]&limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?name=dist/fonts/[name].[ext]' }
     ]
   }
 };
 
 if (!(process.env.NODE_ENV === 'production')) {
+  config.devtool = 'source-map';
   config.plugins = [
     new webpack.DefinePlugin({
       'NODE_ENV': '"dev"'
