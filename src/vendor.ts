960e50b2c0a8eac17d3f1552f9da5a0d0496d667
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-02-25 13:16:30 +0100

    Fixing dependencies for IE

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,18 +1,18 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 ///<reference path="./typings/webpack.d.ts" />
 
+// local shims
+import './ie-shims/ie-shims';
+
 // polyfills
 import 'es6-shim';
 import 'es6-promise';
 
 // (these modules are what is in 'angular2/bundles/angular2-polyfills' so don't use that here)
-import 'es7-reflect-metadata/dist/browser';
+import 'reflect-metadata';
 
 import 'zone.js/dist/zone-microtask';
 import 'zone.js/dist/long-stack-trace-zone';
 
 // Angular 2 Deps
-import 'rxjs';
-
-// local shims
-import './ie-shims/ie-shims';
+import 'rxjs/Rx';
