3af1d312c0a6449bbe3bd025a7df97dcd83edb88
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-07-14 12:32:31 +0200

    Update packages. Angular rc.4.

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,6 +2,6 @@ import { RouterConfig } from '@angular/router';
 import { TestComponent } from './components/test.component';
 
 export const AppRoutes: RouterConfig = [
-  { index: true, component: TestComponent },
+  { path: '', component: TestComponent },
   { path: 'test', component: TestComponent }
 ];
