afaa4030cab0a6647549ffb5c01bb7487d3cd7db
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-09-16 01:06:59 +0200

    Updated to Angular final :rocket:

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'sg-app',
   template: `
     <router-outlet></router-outlet>
-  `,
-  directives: ROUTER_DIRECTIVES
+  `
 })
 export class AppComponent {}
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -7,7 +7,6 @@ if (WEBPACK_ENV === 'production') {
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
